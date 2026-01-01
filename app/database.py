5e545c4be8f5cc7d366f76c4b5fe2e0f79ba4317
y1409channel-svg <y1409channel@gmail.com>
2026-01-01 14:20:30 +0200

    Add database model

--- app/database.py
+++ app/database.py
@@ -1,1 +1,23 @@
+from sqlalchemy import create_engine, Column, Integer, Float, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import datetime
 
+DATABASE_URL = "sqlite:///./cheese_waste.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class WasteRecord(Base):
+    __tablename__ = "waste_records"
+
+    id = Column(Integer, primary_key=True, index=True)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    total_waste = Column(Float)
+    usable_waste = Column(Float)
+    unusable_waste = Column(Float)
+
+Base.metadata.create_all(bind=engine)
